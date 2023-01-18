1007e714e7ada818e2b51b88cda869888e595173
David Strayhorn <david.strayhorn4@gmail.com>
2023-01-18 00:57:15 -0500

    Update core.tsx

--- src/core.tsx
+++ src/core.tsx
@@ -90,14 +90,14 @@ export function NostrProvider({
 
   const reconnectToRelays = useCallback(() => {
     relayUrls.forEach(async (relayUrl) => {
-      var oRelayUrl = {url: relayUrl,id:1}
-      /*
+      var oRelayUrl = {url: relayUrl,status:1}
+
       if (connectedRelays.includes(oRelayUrl)) {
           log(debug, "info", `✅✅==testEdit5=== nostr (${relayUrl}): Still connected!!!`)
       } else {
           log(debug, "info", `❓❓==testEdit5=== nostr (${relayUrl}): Need to reconnect!!!`)
       }
-      */
+      
 
     })
   }, [])
