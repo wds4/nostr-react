8c436639b627a50ea63e784bddd7ee7986e1062b
David Strayhorn <david.strayhorn4@gmail.com>
2023-02-04 16:18:47 -0500

    Update core.tsx

--- src/core.tsx
+++ src/core.tsx
@@ -62,8 +62,9 @@ export function NostrProvider({
   const isFirstRender = useRef(true)
 
   const connectToRelays = useCallback(() => {
+    const aConnectedRelays = [];
     relayUrls.forEach(async (relayUrl) => {
-      console.log("qwerty; connectToRelays for relayUrl: "+relayUrl+"; connectedRelays: "+JSON.stringify(connectedRelays,null,4) );
+      console.log("qwerty; connectToRelays for relayUrl: "+relayUrl+"; connectedRelays: "+JSON.stringify(connectedRelays,null,4) +"; aConnectedRelays: "+JSON.stringify(aConnectedRelays,null,4) );
       if (true) {
         console.log("qwerty; attempting to connect to relayUrl: "+relayUrl);
         const relay = relayInit(relayUrl)
