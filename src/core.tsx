4cb5389c014686e5b088ac05a98d91bc946297cf
David Strayhorn <david.strayhorn4@gmail.com>
2023-02-04 17:00:38 -0500

    Update core.tsx

--- src/core.tsx
+++ src/core.tsx
@@ -69,8 +69,10 @@ export function NostrProvider({
   const connectToRelays = useCallback(() => {
     aConnectedRelays.forEach(async (relayUrl) => {
       if (!relayUrls.includes(relayUrl)) {
-        console.log("qwerty MUST NOW DISCONNECT FROM url: "+relayUrl);
+        console.log("qwerty MUST NOW DISCONNECT from url: "+relayUrl);
         const relay = relayInit(relayUrl)
+        relay.connect()
+        console.log("qwerty connected again to url: "+relayUrl);
         relay.close()
       }
     });
